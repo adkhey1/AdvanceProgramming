02154e9238819dc1b65f021f929f167ea2f7e91c
Simon <simon.carmesin@stud.fra-uas.de>
2022-04-26 17:35:59 +0100

    Initial commit

---
+++ src/main/java/com/example/advanceprogramming/AdvanceProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.advanceprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/advanceprogramming/AdvanceProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advanceprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
