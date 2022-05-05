e4e129b0a0a4cf85cd45965b98da67b7a46b0f78
AndreasKranz <andreas.kranz.22.09@gmail.com>
2022-05-05 10:53:21 +0200

    (work in progress) transform attributes to new table

--- src/main/java/com/example/advanceprogramming/analyze/controller/AnalyzeController.java
+++ src/main/java/com/example/advanceprogramming/analyze/controller/AnalyzeController.java
@@ -3,24 +3,28 @@ package com.example.advanceprogramming.analyze.controller;
 
 import com.example.advanceprogramming.analyze.DTO.FilterDTO;
 import com.example.advanceprogramming.analyze.DTO.MarkerDTO;
+import com.example.advanceprogramming.analyze.model.Attributes;
 import com.example.advanceprogramming.analyze.model.Business;
 import com.example.advanceprogramming.analyze.repository.RestaurantRepository;
 import com.example.advanceprogramming.analyze.service.AnalyzeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class AnalyzeController {
+
+    private static final Logger log = LoggerFactory.getLogger(AnalyzeController.class);
 
     @Autowired
     private RestaurantRepository restaurantRepository;
@@ -60,5 +64,79 @@ public class AnalyzeController {
 
         return ResponseEntity.status(HttpStatus.OK).body(output1);
     }
+
+    @RequestMapping("/transfromdb")
+    public String transfromDB (){
+        ArrayList<String> listRaw = restaurantRepository.selectAllFromBusiness();
+
+        log.debug("Start transfrom with " + listRaw.size()+" entries");
+
+
+        ArrayList<String> keyValue = new ArrayList<>();
+        Attributes attribute;
+
+        String businessID;
+        String attributes;
+        String name;
+        String content;
+
+
+        int firstComma = 0;
+        int nextComma = 0;
+        int openBracket;
+        int closeBracket;
+
+        boolean looper = true;
+        String temp;
+        for (String s: listRaw) {
+            String[] splitted = s.split(",",2);
+            businessID = splitted[0];
+            attributes = splitted[1];
+
+            if (attributes.length() > 2){//Attribute sind vorhanden
+                attributes = attributes.replace("'","");
+                System.out.println("Attributes exist- " + attributes);
+
+               if(!attributes.contains(",")){ //Nur ein Attribut vorhanden
+                    openBracket = attributes.indexOf("{");
+                    closeBracket = attributes.indexOf("}");
+
+                    temp = attributes.substring(openBracket+1,closeBracket);
+                    String[] split = temp.split(": ");
+
+                    name = split[0];
+                    content = split[1];
+
+                   System.out.println("\t" + name);
+                   System.out.println("\t" + content);
+                } else {
+
+                   while (looper) {
+                       firstComma = attributes.indexOf(",",nextComma+1);
+                       nextComma = attributes.indexOf(",",firstComma+1);
+
+                       if(attributes.indexOf("{",firstComma)== -1)
+
+                       attribute = new Attributes();
+
+                        if(nextComma == -1){
+                           looper = false;
+                        }
+                   }
+               }
+
+            } else {//Attribute sind leer
+                System.out.println("No Attributes: " + attributes);
+
+            }
+
+
+        }
+
+
+
+        return "transformingDB";
+    }
+
 
 }
--- src/main/java/com/example/advanceprogramming/analyze/model/Business.java
+++ src/main/java/com/example/advanceprogramming/analyze/model/Business.java
@@ -2,6 +2,7 @@ package com.example.advanceprogramming.analyze.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Getter
 @Entity
 @Table(name = "business")
+@ToString
 public class Business {
 
     @Id
