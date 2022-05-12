35932de5b42946d5fa863735c9a1ec846add24e8
JoelHerold <Delos12>
2022-05-12 12:30:26 +0200

    "3" Window Design - Infos zum geklickten Restaurant + dynamische Chart Erstellung Bsp.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,13 @@
-
+var json_return_marker;
 
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
-        center: {lat: 50.1109, lng: 8.6821}
+        center: {lat: 40.560109, lng: -100.573589}
     })
 
 
-    function addMarker(latLongPos,businessID) {
+    function addMarker(latLongPos, businessID) {
         const marker = new google.maps.Marker({
             position: latLongPos,
 
@@ -20,7 +20,7 @@ function initMap() {
             content: businessID
         });
 
-        marker.addListener("click",()=>{
+        marker.addListener("click", () => {
             detailWindow.open(map, marker);
             $.ajax({
                 'async': "true",
@@ -33,18 +33,26 @@ function initMap() {
                 'data': JSON.stringify({business_id: businessID}),
                 //dataType: "json",
                 'success': function (data) {
-                    console.log("test")
-                    console.log(data)
+                    //console.log("test")
+                    //console.log(data)
+                    json_return_marker = data
                     console.log(businessID)
                 }
             });
+            setTimeout(function (){
+
+                sideView();
+
+            }, 4000);
+
         })
 
     }
+
     var myLatlng;
     for (let i = 0; i < json_data_LatLongArray.length; i++) {
-         myLatlng = new google.maps.LatLng(json_data_LatLongArray[i].latitude, json_data_LatLongArray[i].longitude);
-        addMarker(myLatlng,json_data_LatLongArray[i].business_id)
+        myLatlng = new google.maps.LatLng(json_data_LatLongArray[i].latitude, json_data_LatLongArray[i].longitude);
+        addMarker(myLatlng, json_data_LatLongArray[i].business_id)
 
     }
 
@@ -56,10 +64,8 @@ function initMap() {
 
 
 }
+
 window.initMap = initMap;
-
-
-
 
 
 //get Markers
@@ -67,8 +73,6 @@ var json_data_LatLongArray;
 
 
 function loadMapMarkers() {
-
-
 
 
     $.ajax({
@@ -81,62 +85,191 @@ function loadMapMarkers() {
         }
     });
 
-    /*
-    $.ajax({
-        type: 'POST',
-        url: "/restaurant/filtered/",
-        //force to handle it as text
-        dataType: "JSON",
-        success: function(data) {
-
-            //data downloaded so we call parseJSON function
-            //and pass downloaded data
-
-            //var json = $.parseJSON(data);
-            //now json variable contains data in json format
-            //let's display a few items
-           console.log(data)
-            //json_data_LatLongArray=data;
-          // console.log(json_data_LatLongArray)
-        }
-    });
-*/
 
 }
 
 loadMapMarkers();
 
-console.log(json_data_LatLongArray)
 
 
+console.log(json_return_marker)
 
 
-/*
-function prepJsonForMarker(){
-    for (let i = 0; i < 20; i++) {
-        console.log(json_data_LatLongArray[i].latitude)
-        console.log(json_data_LatLongArray[i].longitude)
+var json_return_markerArrTemp = []
+
+function sideView() {
+
+    //fill the Array
+    if(json_return_markerArrTemp.length<3) {
+        json_return_markerArrTemp.unshift(json_return_marker)
+    }else {
+        json_return_markerArrTemp.pop()
+        json_return_markerArrTemp.unshift(json_return_marker)
     }
 
 
-}
-prepJsonForMarker()
+    //Window1
+    document.getElementById('sideWindow1inner').innerText = JSON.stringify(json_return_markerArrTemp[0]);
+    exampleChart1('chartWindow1');
+    //console.log(chart);
 
-*/
+    //Window2
+    if(json_return_markerArrTemp[1]!=null){
+        document.getElementById('sideWindow2inner').innerText = JSON.stringify(json_return_markerArrTemp[1]);
+        exampleChart2('chartWindow2');
+    }
+
+    //window3
+    if(json_return_markerArrTemp[2]!=null){
+        document.getElementById('sideWindow3inner').innerText = JSON.stringify(json_return_markerArrTemp[2]);
+        exampleChart3('chartWindow3');
+    }
+    console.log(json_return_markerArrTemp)
+
+
+
+}
+
+let myChart1 = null;
+let myChart2 = null;
+let myChart3 = null;
+
+function exampleChart1(div){
+
+
+    const labels = [
+        'Janua',
+        'Februa',
+        'March',
+        'April',
+        'May',
+        'June',
+    ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
+
+    if(myChart1!=null){
+        myChart1.destroy();
+    }
+    myChart1=new Chart(
+        document.getElementById(div),
+        config
+    );
+
+
+
+
+}
+
+function exampleChart2(div){
+
+
+    const labels = [
+        'Janua',
+        'Februa',
+        'March',
+        'April',
+        'May',
+        'June',
+    ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
+
+    if(myChart2!=null){
+        myChart2.destroy();
+    }
+    myChart2=new Chart(
+        document.getElementById(div),
+        config
+    );
+
+
+
+
+}
+
+function exampleChart3(div){
+
+
+    const labels = [
+        'Janua',
+        'Februa',
+        'March',
+        'April',
+        'May',
+        'June',
+    ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
+
+    if(myChart3!=null){
+        myChart3.destroy();
+    }
+    myChart3=new Chart(
+        document.getElementById(div),
+        config
+    );
+
+
+
+
+}
+
+
+///////////////////////////////////////////////////////
 
 //TODO Gracjan Filter
 // document.getElementById("map")
 
 var $items = $('#firstName, #lastName,#phoneNumber,#address ')
 var obj = {}
-$items.each(function() {
+$items.each(function () {
     obj[this.id] = $(this).val();
 })
 
-var json= JSON.stringify( obj);
-
-
-
+var json = JSON.stringify(obj);
 
 
 /*
